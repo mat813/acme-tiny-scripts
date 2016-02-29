f5e95f63a41e833fc247f0c1488ed0423316820c
Mathieu Arnold <mat@mat.cc>
2016-02-29 20:09:11 +0100

    Add a check to make sure all certificates are not expiring too soon.

---
+++ check_expire.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+set -e
+
+. lets_path.sh
+
+STATE_OK=0
+STATE_WARNING=1
+STATE_CRITICAL=2
+STATE_UNKNOWN=3
+STATE_DEPENDENT=4
+
+CRITICAL=25
+WARNING=50
+
+for c in ${lets_public}/*.crt
+do
+	if [ ! -f "${c}" ]
+	then
+		echo "No certificate to check"
+		exit ${STATE_UNKNOWN}
+	fi
+	if ! openssl x509 -in ${c} -noout -checkend $((86400*${WARNING}))
+	then
+		b=`basename ${c} .crt`
+		if ! openssl x509 -in ${c} -noout -checkend $((86400*${CRITICAL}))
+		then
+			warn="${warn} ${b}"
+		else
+			crit="${crit} ${b}"
+		fi
+	fi
+done
+
+if [ -n "${crit}" ]
+then
+	echo "Certificates for${crit} expire in less than ${CRITICAL} days"
+	exit ${STATE_CRITICAL}
+fi
+
+if [ -n "${warn}" ]
+then
+	echo "Certificates for${crit} expire in less than ${WARNING} days"
+	exit ${STATE_WARNING}
+fi
+
+echo "All certificates expire in more than ${WARNING} days."
+exit ${STATE_OK}
