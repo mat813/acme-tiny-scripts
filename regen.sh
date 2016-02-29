b4effaad37f8ee6ea61bdc9b12d49a1a0c386f48
Mathieu Arnold <mat@mat.cc>
2016-02-29 20:18:27 +0100

    Add a regen script.

---
+++ regen.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+set -e
+
+. lets_path.sh
+
+if [ $# -lt 1 ]
+then
+	echo "Usage: $0 [services...]"
+	exit 1
+fi
+
+if [ ! -e ${lets_private}/account.key ]
+then
+	echo "Account key does not exist : ${lets_private}/account.key"
+	echo "Maybe run ./gen_key.sh account"
+	exit 1
+fi
+
+for c in ${lets_public}/*.crt
+do
+	if [ ! -f "${c}" ]
+	then
+		echo "No certificate to renew"
+		exit 1
+	fi
+	b=`basename ${c} .crt`
+	./gen_one.sh ${b}
+done
+
+for s in $@
+do
+	sudo /usr/sbin/service ${s} reload
+done
