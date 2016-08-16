1421e1e44804b749f8ba215149a138505771967d
Mathieu Arnold <mat@mat.cc>
2016-08-16 09:57:54 +0100

    Add HTTPS TLSA entries generation.

--- gen_csr.sh
+++ gen_csr.sh
@@ -32,6 +32,14 @@ then
 	exit 1
 fi
 
+tlsa_hash=$(openssl rsa -in "${key_name}" -outform DER -pubout 2>/dev/null | openssl dgst -sha256 -binary | hexdump -ve '1/1 "%02x"')
+
+> "${lets_public}/${name}.tlsa"
+
+tlsa_line() {
+	printf '_443._tcp.%s. IN TLSA 3 1 1 %s\n' "${1}" "${tlsa_hash}" >> "${lets_public}/${name}.tlsa"
+}
+
 config=$(mktemp)
 trap 'rm -f ${config}' EXIT
 
@@ -41,11 +49,14 @@ echo "[SAN]" >> "${config}"
 
 output="subjectAltName=DNS:$1"
 
+tlsa_line "$1"
+
 shift
 
 for d in "$@"
 do
 	output="${output},DNS:${d}"
+	tlsa_line "${d}"
 done
 
 echo "${output}" >> "${config}"
