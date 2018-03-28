d63fe6bfd50b1dd7f4160574e2f7c597ed1e4281
Mathieu Arnold <mat@mat.cc>
2018-03-28 14:43:11 +0200

    Add dns slave certificates renewal.

--- gen_one_dns.sh
+++ gen_one_dns.sh
@@ -16,10 +16,15 @@ name=$1
 
 shift
 
+key_name=${lets_private}/${name}.key
+csr_name=${lets_private}/${name}.csr
+
 if [ -x "${dns_auth_hook}" ] && [ -x "${dns_cleanup_hook}" ]
 then
-	key_name=${lets_private}/${name}.key
-	csr_name=${lets_private}/${name}.csr
+	crt_name=${lets_public}/${name}.crt
+	chain_name=${lets_public}/${name}.chain
+	bundle_name=${lets_public}/${name}.bundle
+	txt_name=${lets_public}/${name}.txt
 
 	if [ ! -e "${lets_private}/account.key" ]
 	then
@@ -72,12 +77,37 @@ then
 		--chain-path "${cert}.chain" \
 		--fullchain-path "${cert}.bundle"
 
-	cat "${cert}" > "${lets_public}/dns/${name}.crt"
-	cat "${cert}.chain" > "${lets_public}/dns/${name}.chain"
-	cat "${cert}.bundle" > "${lets_public}/dns/${name}.bundle"
+	cat "${cert}" > "${crt_name}"
+	cat "${cert}.chain" > "${chain_name}"
+	cat "${cert}.bundle" > "${bundle_name}"
 
-	openssl x509 -noout -text -in "${cert}" > "${lets_public}/dns/${name}.txt"
+	openssl x509 -noout -text -in "${cert}" > "${txt_name}"
+elif [ -n "${dns_url}" ] && [ -n "${dns_login}" ] && [ -n "${dns_password}" ]
+then
+	crt_name=${lets_public}/dns/${name}.crt
+	chain_name=${lets_public}/dns/${name}.chain
+	bundle_name=${lets_public}/dns/${name}.bundle
+	txt_name=${lets_public}/dns/${name}.txt
+	tlsa_name=${lets_public}/${name}.tlsa
+	for ext in key crt chain bundle tlsa
+	do
+		tmp=$(mktemp)
+		#--header 'If-Modified-Since: Tue, 27 Mar 2018 14:31:08 GMT'
+		if curl --fail --silent -o "${tmp}"  --user "${dns_login}:${dns_password}"  "${dns_url}/${name}.${ext}"
+		then
+			eval file='${'${ext}'_name}'
+			if ! cmp -s "${tmp}" "${file}"
+			then
+				mkdir -p "$(dirname "${file}")"
+				mv "${tmp}" "${file}"
+			else
+				rm -f "${tmp}"
+			fi
+		else
+			echo "${name}.${ext} not found"
+		fi
+	done
 else
-	echo "One of the hooks is not executable."
+	echo "Either set the hook to executables or set dns_url/login/password"
 	exit 1
 fi
